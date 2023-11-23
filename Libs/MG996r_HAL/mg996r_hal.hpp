ed522cfcd2c572f311e1d7aa7530488db56f0a8a
DuPont <SEMIRocketBoy@gmail.com>
2023-11-23 09:53:04 +0300

    feat: add new lib files for MG996r

---
+++ Libs/MG996r_HAL/mg996r_hal.hpp
@@ -0,0 +1,20 @@
+#ifndef __MG996R_HAL
+#define __MG996R_HAL
+
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32g4xx_hal.h"
+
+/* Declarations and definitions ----------------------------------------------*/
+class mg996r_hal
+{
+private:
+    /* data */
+    
+public:
+    mg996r_hal(/* args */);
+
+};
+
+
+#endif /* __MG996R_HAL */
